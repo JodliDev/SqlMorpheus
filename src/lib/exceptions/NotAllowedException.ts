534eef677ada8658ccb3c2b45e9562d7b89c1c60
JodliDev <jodlidev@gmail.com>
2025-08-10 12:48:45 +0200

    Improve error message

--- src/lib/exceptions/NotAllowedException.ts
+++ src/lib/exceptions/NotAllowedException.ts
@@ -2,7 +2,7 @@ import {NotAllowedChangeEntry} from "../Migrations";
 
 export default class NotAllowedException extends Error {
 	constructor(entries: NotAllowedChangeEntry[]) {
-		let message = "Some destructive changes need to be confirmed. In preMigration() run:\n";
+		let message = "Some destructive changes need to be confirmed. To confirm, add to preMigration():\n";
 		for(const entry of entries) {
 			message += `\t\tmigrations.allowMigration(${entry.version}, ${entry.tableName}, "${entry.type}");\n`;
 		}
