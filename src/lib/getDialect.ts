b1541a3e91e77210d2e130a9aa9954ec4b622d58
JodliDev <jodlidev@gmail.com>
2026-02-07 19:10:30 +0100

    Remove MsSql support

--- src/lib/getDialect.ts
+++ src/lib/getDialect.ts
@@ -1,6 +1,5 @@
 import SqliteDialect from "./dialects/SqliteDialect";
 import PostgresDialect from "./dialects/PostgresDialect";
-import MsSqlDialect from "./dialects/MsSqlDialect";
 import MySqlDialect from "./dialects/MySqlDialect";
 import DatabaseInstructions from "./typings/DatabaseInstructions";
 import {DatabaseAccess} from "./typings/DatabaseAccess";
@@ -12,8 +11,6 @@ export default function getDialect(db: DatabaseAccess, dbInstructions: DatabaseI
 			return new SqliteDialect(db);
 		case "Postgres":
 			return new PostgresDialect(db);
-		case "MsSql":
-			return new MsSqlDialect(db);
 		case "MySql":
 			return new MySqlDialect(db);
 		default:
